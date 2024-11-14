3d6825d805f8c7d79b60d9556b895d52a74c4515
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-11-14 17:18:27 +0100

    update

--- main.py
+++ main.py
@@ -1,195 +1,170 @@
-### Diese Datei main.py soll bearbeitet (editiert) werden.
-### Wenn für die Lösung einer Übungsaufgabe eine Datei abgegeben werden soll, dann diese!
-import pygame
+from time import sleep
 
 import sokoban
 
-# Benutzen Sie für die Abgaben Ihre eigene Matrikelnummer.
-# Jede Matrikelnummer erzeugt eine andere Welt.
-# In seltenen Fällen ist ihr Spiel nicht lösbar weil die z.B. Box am Rand steht. 
-# Wenden sie sich in diesem Fall an den Dozenten um eine alternativen Startwert zu erhalten.
+# Ergebnisse für 20x20-Spielbrett
+str_a = "s0123456"  # RRRRRRUUUUUULLLDLU
+str_b = "s0596553"  # DDDDDDDDRRRRRRRRRRDRUUU
+str_c = "s0596553a" # RRDLLLLLLLLLLLLLDLUUUU
+str_d = "s0596553b" # RRRRRRRRRRRRRRRRDDDDDLLLLLLULDDDD
+str_e = "s0596553c" # LLDDDRRRRRRRDRUUUUUUUU
+str_f = "s0596553d" # Dieses Spiel kann leider nicht gewonnen werden
+str_g = "s0596553e" # URRDLLLLULD
+str_h = "s0596553f" # RRRRRRRRDDDRRRRRRRRRURDDDDDDDDDDDDD
+str_i = "s0596553g" # LLLLLLLLLLLLLLLLLDDDDDDDDDDDLUUUUUUUUUUUUUU
+str_j = "s0596553h" # LLLLLLLLLLLUUUUUURRRRRRDRUUUUUUUU
+str_k = "s0596553q" # Dieses Spiel kann leider nicht gewonnen werden
 
-# Die hier angegebene Matrikelnummer erzeugt nur meine Referenzwelt die auch in der Doku verwendet wird, bitte ändern.
+# World instantiieren
+s = sokoban.World(str_a)
 
-#s = sokoban.World("s0123456")
-# using seed: s0123456 moves: RRRRRRUUUUUULLLDLU
-
-#s = sokoban.World("s0596553")
-# using seed: s0596553 moves: DDDDDDDDRRRRRRRRRRDRUUU
-
-#s = sokoban.World("s0596553a")
-# using seed: s0596553a moves: RRDLLLLLLLLLLLLLDLUUUU
-
-#s = sokoban.World("s0596553b")
-# using seed: s0596553b moves: RRRRRRRRRRRRRRRRDDDDDLLLLLLULDDDD
-
-#s = sokoban.World("s0596553c")
-# using seed: s0596553c moves: LLDDDRRRRRRRDRUUUUUUUU
-
-#s = sokoban.World("s0596553d")
-# using seed: s0596553d moves:
-# Spiel kann nicht gewonnen werden!
-
-#s = sokoban.World("s0596553e")
-# using seed: s0596553e moves: URRDLLLLULD
-
-#s = sokoban.World("s0596553f")
-# using seed: s0596553f moves: RRRRRRRRDDDRRRRRRRRRURDDDDDDDDDDDDD
-
-s = sokoban.World("s0596553g")
-# using seed: s0596553g moves: DDDDDDDDDDDLLLLLLLLLLLLLLLLLLUUUUUUUUUUUUUU
-
-#s = sokoban.World("s0596553h")
-# using seed: s0596553h moves: LLLLLLLLLLLUUUUUURRRRRRDRUUUUUUUU
-
-### BEGIN fügen sie unter dieser Zeile ihren Code zur Lösung ein.
-
-# Konstanten für die relative Position.
-ABOVE = LEFT = 1
-BELOW = RIGHT = -1
+# Konstanten zur relativen Bestimmung
+LEFT = ABOVE = -1
+RIGHT = BELOW = 1
 HIT = 0
 
-# Funktionen zur relativen Distanzbestimmung.
-def x_distance(source: sokoban.Cell = s.box, target: sokoban.Cell = s.target) -> int:
-    if source.x - target.x > 0: return LEFT
-    if source.x - target.x == 0: return HIT
-    if source.x - target.x < 0: return RIGHT
+# Polymorphie ausnutzen, daher spezialisierte Unterklassen.
+class Target(sokoban.Cell):
+    def __init__(self, x=None, y=None):
+        super().__init__(x, y)
+
+# cast Cell als Target-Instanz
+s.target = Target(s.target.x, s.target.y)
+
+def is_winnable():
+    """
+    Überprüft, ob das aktuelle Spiel gewonnen werden kann.
+    :return: False, wenn Spiel nicht gewonnen werden kann, ansonsten True
+    """
+    pos = calculate_rel_pos(s.target, s.box)
+
+    # Ziel [Richtung] von Box, Box jedoch am anderen Rand:
+    if pos[0] == LEFT and s.box.x == s.w - 1: return False
+    if pos[0] == RIGHT and s.box.x == 0: return False
+    if pos[1] == ABOVE and s.box.y == s.h - 1: return False
+    if pos[1] == BELOW and s.box.y == 0: return False
+
+    # Ziel ganz oben:
+    if s.target.y == 0:
+        # Box links, Ziel nicht
+        if s.box.x == 0 and s.target.x != 0: return False
+        # Box rechts, Ziel nicht
+        if s.box.x == s.w - 1 and s.target.x != s.w - 1 : return False
+
+    # Ziel ganz unten:
+    if s.target.y == s.h - 1:
+        # Box links, Ziel nicht
+        if s.box.x == 0 and s.target.x != 0: return False
+        # Box rechts, Ziel nicht
+        if s.box.x == s.w - 1 and s.target.x != s.w - 1 : return False
+    return True
 
 
-def y_distance(source: sokoban.Cell = s.box, target: sokoban.Cell = s.target) -> int:
-    if source.y - target.y > 0: return ABOVE
-    if source.y - target.y == 0: return HIT
-    if source.y - target.y < 0: return BELOW
+def calculate_rel_pos(a: sokoban.Cell, b: sokoban.Cell):
+    """
+    Berechnet die relative Position einer Zelle in Bezug auf die Zielzelle.
+    :param a: Zielzelle
+    :param b: Zelle, deren Position ermittelt werden soll
+    :return: list[px, py] (siehe Positionskonstanten)
+    """
+    dx = a.x - b.x
+    dy = a.y - b.y
+
+    output = []
+
+    match dx:
+        case dx if dx < HIT: output.append(LEFT)
+        case dx if dx > HIT: output.append(RIGHT)
+        case _:
+            if b.x == 0: output.append(LEFT)
+            else: output.append(RIGHT)
+
+    match dy:
+        case dy if dy < HIT: output.append(ABOVE)
+        case dy if dy > HIT: output.append(BELOW)
+        case _:
+            if b.y == 0: output.append(ABOVE)
+            else: output.append(BELOW)
+
+    return output
 
 
+def step_out():
+    """
+    Spieler versetzen, falls in einer Linie mit Box. Erforderlich für Vektorlauf.
+    :return: None
+    """
+    if s.box.x - s.me.x == HIT:
+        if s.box.x > 0: s.left()
+        else: s.right()
+    if s.box.y - s.me.y == HIT:
+        if s.box.y > 0: s.up()
+        else: s.down()
 
 
-# Überprüfen, ob Spiel überhaupt gewonnen werden kann.
-def unwinnable() -> bool:
-    # Box ist am unteren Rand, Ziel ist jedoch oberhalb Box
-    if y_distance() == ABOVE and s.box.y == s.SIZE - 1: return True
-    # Box ist am oberen Rand, Ziel ist jedoch unterhalb Box
-    if y_distance() == BELOW and s.box.y == 0: return True
-    # Box ist am rechten Rand, Ziel ist jedoch links von Box
-    if x_distance() == LEFT and s.box.x == s.SIZE - 1: return True
-    # Box ist am linken Rand, Ziel ist jedoch rechts von Box
-    if x_distance() == RIGHT and s.box.x == 0: return True
-    return False
+def run_vector(a: sokoban.Cell, b: sokoban.Cell, px: int=HIT, py: int=HIT):
+    """
+    Bewegt Spieler entlang des Vektors b->a.
+    :param a: Zielzelle
+    :param b: Startzelle
+    :param px: relative x-Position
+    :param py: relative y-Position
+    :return: None
+    """
+    tx = dx = a.x - b.x
+    ty = dy = a.y - b.y
+
+    while dx != px:
+        match dx:
+            case dx if dx < px:
+                s.left()
+                dx += 1
+            case dx if dx > px:
+                s.right()
+                dx -= 1
+            case _: break
+
+    if s.has_target(s.box): return
+
+    if isinstance(a, Target):
+        if ty < py: s.down()
+        if ty > py: s.up()
+        if tx < px: s.left()
+        if tx > px: s.right()
+
+        if dy != HIT:
+            if s.box.x - s.me.x < HIT: s.left()
+            if s.box.x - s.me.x > HIT: s.right()
+
+    while dy != py:
+        match dy:
+            case dy if dy < py:
+                s.up()
+                dy += 1
+            case dy if dy > py:
+                s.down()
+                dy -= 1
+            case _: break
 
 
-## Einmaliger Aufruf:
-# Prüfen, ob Welt unlösbar ist
-if unwinnable():
-    print("Spiel kann nicht gewonnen werden!")
-    font = pygame.font.SysFont(None, 100)
-    text = font.render("Unlösbar!", True, sokoban.WHITE)
-    s.screen.blit(text, (
-        s.screen.get_width() // 2 - text.get_width() // 2, s.screen.get_height() // 2 - text.get_height() // 2))
-    pygame.display.flip()
-    pygame.time.delay(3000)
+# Spiel beginnen
+
+# relative Position der Box zum Ziel ermitteln
+target_pos = calculate_rel_pos(s.target, s.box)
+
+# kurz warten
+sleep(1)
+
+# Spieler ggf. versetzen
+step_out()
+
+# Gewinnchancen überprüfen
+if not is_winnable():
+    print("Dieses Spiel kann leider nicht gewonnen werden.", end="")
+    sleep(3)
     exit(0)
 
-# Spieler ausrücken, wenn auf einer Spalte mit Box
-if x_distance(s.me, s.box) == HIT:
-    if s.me.x > 0:
-        s.left()
-    else:
-        s.right()
-
-# Spieler ausrücken, wenn in einer Zeile mit Box
-if y_distance(s.me, s.box) == HIT:
-    if s.me.y > 0:
-        s.up()
-    else:
-        s.down()
-
-## Schleife, bis Spiel gewonnen ist:
+# Game-Loop
 while not s.winning():
-
-    # Aufholen L/R
-    while True:
-        # Ziel ist links von Box
-        if x_distance() == LEFT:
-            # Spieler nach links bewegen, wenn Box weiter links von Spieler
-            if s.me.x - s.box.x > LEFT: s.left()
-            # Spieler nach rechts bewege, wenn Box weniger links (also rechts) von Spieler
-            if s.me.x - s.box.x < LEFT: s.right()
-            # Spieler nicht mehr bewegen, wenn Box genau links neben Spieler
-            if s.me.x - s.box.x == LEFT: break
-        # Ziel ist rechts von Box
-        if x_distance() == RIGHT:
-            # Spieler nach links bewegen, wenn Box weiter links von Spieler
-            if s.me.x - s.box.x > RIGHT: s.left()
-            # Spieler nach rechts bewegen, wenn Box weniger links (also rechts) von Spieler
-            if s.me.x - s.box.x < RIGHT: s.right()
-            # Spieler nicht mehr bewegen, wenn Box genau rechts neben Spieler
-            if s.me.x - s.box.x == RIGHT: break
-        # Ziel ist über oder unter Box
-        if x_distance() == HIT: break
-
-    # Aufholen U/D
-    while True:
-        # Wenn Spieler unterhalb Box
-        if s.me.y - s.box.y > HIT: s.up()
-        # Wenn Spieler oberhalb Box
-        if s.me.y - s.box.y < HIT: s.down()
-        # Spieler anhalten, wenn auf gleicher Höhe mit Box
-        if s.me.y - s.box.y == HIT: break
-
-    # Schub der Box L/R
-    while True:
-        # Wenn Box rechts von Ziel
-        if s.box.x - s.target.x > HIT: s.left()
-        # Wenn Box links von Ziel
-        if s.box.x - s.target.x < HIT: s.right()
-        # Wenn Box ober- oder unterhalb von Ziel
-        if s.box.x - s.target.x == HIT: break
-
-    # Um die Ecke laufen:
-    # Spieler nach unten bewegen, wenn Ziel oberhalb Box
-    if y_distance() == ABOVE: s.down()
-    # Spieler nach oben bewegen, wenn Ziel unterhalb Box
-    if y_distance() == BELOW: s.up()
-    # Spieler nach links bewegen, wenn Box links von Spieler
-    if x_distance(s.me, s.box) == LEFT: s.left()
-    # Spieler nach rechts bewegen, wenn Box rechts von Spieler
-    if x_distance(s.me, s.box) == RIGHT: s.right()
-
-    # Zur Box aufholen L/R (falls nötig)
-    while True:
-        # Wenn Spieler rechts von Box
-        if s.me.x - s.box.x > HIT: s.left()
-        # Wenn Spieler links von Box
-        if s.me.x - s.box.x < HIT: s.right()
-        if s.me.x - s.box.x == HIT: break
-
-    # Schub der Box U/D
-    while True:
-        # Wenn Ziel oberhalb von Box, Spieler nach oben bewegen
-        if y_distance(s.box, s.target) == ABOVE: s.up()
-        # Wenn Ziel unterhalb von Box, Spieler nach unten bewegen
-        if y_distance(s.box, s.target) == BELOW: s.down()
-        # Wenn Box auf Höhe von Ziel, Spieler stoppen
-        if y_distance(s.box, s.target) == HIT: break
-
-### END
-
-# Idealerweie ist die Box jetzt auf dem Target und das Spiel gewonnen.
-# Falls (noch) nicht, können sie den Sokoban mit der Tastatur (Pfeiltasten) bewegen.
-# Ändern Sie ab hier nichts mehr. 
-
-# manual keyboard control
-# while not s.winning():
-#     k = s.waitKey()
-#     if k=='q':
-#         break
-#     if k == 'w':
-#         s.up()
-#     if k == 'a':
-#         s.left()
-#     if k == 's':
-#         s.down()
-#     if k == 'd':
-#         s.right()
-
-# ende, sie haben gewonnen
+    run_vector(s.box, s.me, target_pos[0])
+    run_vector(s.target, s.box)
